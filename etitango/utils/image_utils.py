4bef96127453cf85b3999f91008dc197cdda5da7
spanta <santiagopantaleo@gmail.com>
2020-08-29 11:18:38 -0300

    Commentaries added

--- etitango/utils/image_utils.py
+++ etitango/utils/image_utils.py
@@ -2,21 +2,23 @@ from PIL import Image
 from io import BytesIO
 from django.core.files import File
 
+#RECEIVES AN IMAGE PATH AND CHANGES IMAGE SIZE
 def reduce_image_size(image, new_name=None, new_quality=None, new_size=None):
     with Image.open(image) as img:
-        thumb_io = BytesIO()
-
-        if img.mode in ("RGBA", "P"):
+        thumb_io = BytesIO() #Later load image in bytedata. Necesary to link PIL image with django file
+        
+        #JPG doesn't have alfa property. For this files is necesary to convert them
+        if img.mode in ("RGBA", "P"): 
             rgb_img = img.convert("RGB")
             img = rgb_img
 
-        if new_name:
+        if new_name: #If you want to change the image name
             image_name = new_name    
         else:
             image_name = image.name
 
         if new_size:
-            img.thumbnail(new_size, Image.ANTIALIAS)
+            img.thumbnail(new_size, Image.ANTIALIAS) #Like resize but mantains image aspect ratio
             img.save(thumb_io, format='JPEG')
 
         elif new_quality:
@@ -26,6 +28,7 @@ def reduce_image_size(image, new_name=None, new_quality=None, new_size=None):
 
         return new_image
 
+#NOT IMPLEMENTED YET
 def verify_image_file(image, valid_image_format=None):
     valid_format = False
     image_format = None
