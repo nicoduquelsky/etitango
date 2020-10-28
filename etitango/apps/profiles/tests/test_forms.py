f8ddbf28b1c486c879917b29ff30986d816c3166
spanta <santiagopantaleo@gmail.com>
2020-10-28 11:24:40 -0300

    Tests forms

--- etitango/apps/profiles/tests/test_forms.py
+++ etitango/apps/profiles/tests/test_forms.py
@@ -1,35 +1,55 @@
-# import datetime
+from datetime import datetime
 
-# from django.test import TestCase
-# from django.utils import timezone
+from django.test import SimpleTestCase, TestCase
+from django.utils import timezone
 
-# from catalog.forms import RenewBookForm
+from apps.profiles.forms import UserForm, ProfileForm, RegisterForm
 
-# class RenewBookFormTest(TestCase):
-#     def test_renew_form_date_field_label(self):
-#         form = RenewBookForm()
-#         self.assertTrue(form.fields['renewal_date'].label == None or form.fields['renewal_date'].label == 'renewal date')
+profile_form_data = {'name':'Test Name', 'last_name':'Last Last', 'dni_type':'DNI',
+                    'dni_number':'33655465','birth_date':datetime(1980, 12, 31),
+                    'gender':'M', 'country':1, 'province':1, 'province':1}
 
-#     def test_renew_form_date_field_help_text(self):
-#         form = RenewBookForm()
-#         self.assertEqual(form.fields['renewal_date'].help_text, 'Enter a date between now and 4 weeks (default 3).')
-
-#     def test_renew_form_date_in_past(self):
-#         date = datetime.date.today() - datetime.timedelta(days=1)
-#         form = RenewBookForm(data={'renewal_date': date})
-#         self.assertFalse(form.is_valid())
-
-#     def test_renew_form_date_too_far_in_future(self):
-#         date = datetime.date.today() + datetime.timedelta(weeks=4) + datetime.timedelta(days=1)
-#         form = RenewBookForm(data={'renewal_date': date})
-#         self.assertFalse(form.is_valid())
-
-#     def test_renew_form_date_today(self):
-#         date = datetime.date.today()
-#         form = RenewBookForm(data={'renewal_date': date})
-#         self.assertTrue(form.is_valid())
+# class UserAdminFormTest(SimpleTestCase):
+#     def setUpClass(self):
+#         self.user_form = RegisterForm(data={'email':'test@user.com', 
+#                                     'password1':'1234', 
+#                                     'password2':'1234'})
+    
+#     def test_user_form_password_label(self):
+#         label_pass1 = self.user_form.fields['password1'].label == 'Contraseña'
+#         label_pass2 = self.user_form.fields['password2'].label == 'Confirmar Contraseña'
         
-#     def test_renew_form_date_max(self):
-#         date = timezone.localtime() + datetime.timedelta(weeks=4)
-#         form = RenewBookForm(data={'renewal_date': date})
-#         self.assertTrue(form.is_valid())
+#         self.asserTrue(label_pass1 and label_pass2)
+
+# class UserAdminFormTest(SimpleTestCase):
+#     @classmethod
+#     def setUpClass(self):
+#         self.user_form = RegisterForm(data={'email':'test@user.com', 
+#                                     'password1':'1234', 
+#                                     'password2':'1234'})
+    
+#     def test_user_form_password_label(self):
+#         label_pass1 = self.user_form.fields['password1'].label == 'Contraseña'
+#         label_pass2 = self.user_form.fields['password2'].label == 'Confirmar Contraseña'
+        
+#         self.asserTrue(label_pass1 and label_pass2)
+
+class UserProfileFormTest(TestCase):
+    def test_user_form_email_label(self):
+        user_form = UserForm()
+        label_email = user_form.fields['email'].label == 'Email'
+
+        self.assertTrue(label_email)
+
+    # def test_proper_profile_form(self):
+    #     self.databases = '__all__'
+    #     profile_form = ProfileForm(data=profile_form_data)
+    #     self.assertTrue(profile_form.is_valid())
+    
+    # def test_actual_date_of_birth(self):
+    #     actual_date = datetime.now()
+    #     profile_form_data['birth_date'] = actual_date
+    #     profile_form = ProfileForm(data=profile_form_data)
+                
+    #     self.assertFalse(profile_form.is_valid())
+
