5bcc344c109b325cbde193980749bfa5e49e33ef
Schrott Gerardo <schrottgerardo@protonmail.com>
2020-11-01 12:31:20 -0300

    Changing dev and test, we gonna got every enviroment in all branchs

---
+++ scripts/docker-entrypoint-test.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+
+# Collect static files
+echo "===============Collect static files==============="
+python manage.py collectstatic --noinput
+
+# Apply database migrations
+echo "===============Apply database migrations==============="
+python manage.py makemigrations profiles blog pages events expenditure countries
+python manage.py migrate countries --fake
+python manage.py migrate
+
+# Start server
+echo "===============Starting server==============="
+python3 manage.py test
